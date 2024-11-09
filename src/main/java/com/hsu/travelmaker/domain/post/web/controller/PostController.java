db0c9fad048e756807852ff6ed5d448a4270fb0e
jungeun-e <lucycy255@gmail.com>
2024-11-09 14:47:11 +0900

    ✨ PostController 작성

---
+++ src/main/java/com/hsu/travelmaker/domain/post/web/controller/PostController.java
@@ -0,0 +1,24 @@
+package com.hsu.travelmaker.domain.post.web.controller;
+
+import com.hsu.travelmaker.domain.post.web.dto.PostCreateDto;
+import com.hsu.travelmaker.global.response.CustomApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/post")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostServiceImpl postService;
+
+    @PostMapping("/create")
+    public ResponseEntity<CustomApiResponse<?>> createPost(@RequestBody PostCreateDto dto) {
+        return postService.createPost(dto);
+    }
+
+}
