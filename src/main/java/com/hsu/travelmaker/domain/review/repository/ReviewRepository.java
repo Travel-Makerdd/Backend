9f0b2d6802b3ca061cb0b31b205b53be4eafd66f
hs-2171001-namgungmin <115778281+hs-2171001-namgungmin@users.noreply.github.com>
2024-11-21 02:04:00 +0900

    ✨ 여행상품 리뷰 조회 기능 구현

--- src/main/java/com/hsu/travelmaker/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/hsu/travelmaker/domain/review/repository/ReviewRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    // 여행상품과 연관된 리뷰 목록 조회
+    List<Review> findByTripId(Trip trip);
 
     // 사용자와 여행 상품에 해당하는 리뷰 조회
     Optional<Review> findByUserIdAndTripId(User userId, Trip tripId);
